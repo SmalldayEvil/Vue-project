84406c4c1c49a3949d43a51a9f485d109e0c80e1
small <854123852@qq.com>
2018-11-22 22:02:31 +0800

    '商品列表渲染完成'

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/site/css/style.css'
+import VueRouter from 'vue-router'
+// 模块化导入必须加上下面这句
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false
 
+import proList from './components/proList.vue'
+
+let routes = [
+  {path:'/',redirect:'/proList'},
+  {path:'/proList',component:proList}
+]
+
+let router = new VueRouter({
+  routes
+})
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
